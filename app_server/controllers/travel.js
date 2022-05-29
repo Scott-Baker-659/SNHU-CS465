c8bafa33a201a90e37e7dcb535d1849c6e412356
Scott Baker <hyrulehero56@gmail.com>
2022-05-29 14:13:24 -0400

    Created object schema with Mongoose, populate MongoDB

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
+    pageTitle = process.env.TRAVEL_TITLE + ' - Travel';
     res.render('travel', {title: pageTitle, trips});
 };
 
