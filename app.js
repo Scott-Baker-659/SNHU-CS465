67cfcf768d880035cc860061d3f6d8be6139d541
Scott Baker <hyrulehero56@gmail.com>
2022-06-19 21:13:22 -0400

    working week 5

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+require("./app_api/database/db");
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+const apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'app_server' , 'views'));
 
-  // register handlebars partials (hhtps://www.npmjs.com/package/hbs)
-  hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+// register handlebars partials (https://www.npmjs.com/package/hbs) 
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
